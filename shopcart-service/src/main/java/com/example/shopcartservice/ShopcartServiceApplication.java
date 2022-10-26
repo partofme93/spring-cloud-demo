1777be4bd1d19d95ed49bcb22fbd8ebb849a958e
wunanfang <zhuifeng@douyu.tv>
2022-10-27 00:47:17 +0800

    structure change

---
+++ shopcart-service/src/main/java/com/example/shopcartservice/ShopcartServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.shopcartservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopcartServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopcartServiceApplication.class, args);
+    }
+
+}
---
+++ shopcart-service/src/test/java/com/example/shopcartservice/ShopcartServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shopcartservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopcartServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
